df72111504dc209890823f2012f39cd8f1288585
Jonathan <jonathanguven@gmail.com>
2023-12-13 18:30:07 -0800

    implement GET request of flashcard endpoint

--- server/src/api/flashcards.js
+++ server/src/api/flashcards.js
@@ -36,12 +36,36 @@ router.post('/', authenticate, async (req, res) => {
 	}
 });
 
-//   // Get a specific flashcard set
-// router.get('/:id', authenticate, async (req, res) => {
-// 	const id = req.id; 
-// 	const setId = req.params.id;
-// 	// ... logic to get the flashcard set if id matches ...
-// });
+// Get a specific flashcard set
+router.get('/:id', authenticate, async (req, res) => {
+	const userId = req.id; 
+	const setId = req.params.id;
+	try {
+		const { data: flashcardSet, error } = await supabase
+			.from('flashcard_sets')
+			.select()
+			.eq('id', setId)
+			.single();
+		
+		if (error) throw error;
+
+		// no flashcard set found
+		if (!flashcardSet) {
+			return res.status(404).json({ message: 'Flashcard set not found '})
+		}
+
+		// check for public or if user is owner
+		if (!flashcardSet.is_public && flashcardSet.user_id !== userId) {
+			return res.status(403).json({ message: 'You do not have access to this page!' });
+		}
+
+		// return flashcard set
+		res.status(200).json({ flashcardSet });
+
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
+});
 
 //   // Update a specific flashcard set
 // router.put('/:id', authenticate, async (req, res) => {
