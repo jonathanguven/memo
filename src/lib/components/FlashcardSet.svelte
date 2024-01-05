6247b7399327daf788ecde8c16be8b3834ef0bcd
Jonathan <jonathanguven@gmail.com>
2024-01-04 16:48:08 -0800

    padding change

--- src/lib/components/FlashcardSet.svelte
+++ src/lib/components/FlashcardSet.svelte
@@ -41,7 +41,7 @@
     afterUpdate(() => {
         checkOverflow();
     });
-    
+
     // Reactive statements to update classes based on overflow
     $: titleClass = isOverflowing(titleElement) ? 'items-start' : 'items-center';
     $: descriptionClass = isOverflowing(descriptionElement) ? 'items-start' : 'items-center';
@@ -98,7 +98,7 @@
 
 <div>
     <button 
-        class="flashcard relative border-2 p-12 flex items-center justify-center bg-zinc-800 shadow-xl rounded-xl cursor-pointer" 
+        class="flashcard relative border-2 py-8 px-12 flex items-center justify-center bg-zinc-800 shadow-xl rounded-xl cursor-pointer" 
         on:click={redirect(id)}
     >
         <div class="wrap flex justify-center text-3xl w-full min-h-full {titleClass}" bind:this={titleElement}>
