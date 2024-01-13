ff9107a166517337a7d55ae38f0401470836457e
Jonathan <jonathanguven@gmail.com>
2024-01-13 02:31:23 -0800

    ui update

--- src/lib/routes/CreateAccount.svelte
+++ src/lib/routes/CreateAccount.svelte
@@ -160,4 +160,10 @@
         color: #242424;
         padding: 6px 12px
     }
+    input:-webkit-autofill,
+    input:-webkit-autofill:hover, 
+    input:-webkit-autofill:focus, 
+    input:-webkit-autofill:active{
+        -webkit-box-shadow: 0 0 0 30px white inset !important;
+    }
 </style>
